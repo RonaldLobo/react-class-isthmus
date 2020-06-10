ebbf3d06e0e8440ea117f0d769e266687f5f624f
Ronald Lobo <rlb@akc.org>
2020-06-10 12:42:59 -0600

    initial

---
+++ build/precache-manifest.81dca8f0ebe4078177966656191231ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25eb84e8e4e0e886dbbb1f0bb3c1db99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f469961869857fd52a66",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "02a4c8f994f039de0f4c",
+    "url": "/static/js/2.52f92dc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52f92dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f469961869857fd52a66",
+    "url": "/static/js/main.c892bd78.chunk.js"
+  },
+  {
+    "revision": "51cea64317322dbd872d",
+    "url": "/static/js/runtime-main.0c21f40b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
